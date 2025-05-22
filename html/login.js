38b370974ce491cdead29b0e21465c3cf9154205
kurt.hsu <kurt.hsu@nextbank.com.tw>
2025-05-22 11:25:41 +0800

    add domain

--- html/login.js
+++ html/login.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://3.86.236.95:8080';
+const BASE_URL = 'https://www.maplestoryexchange.com';
 // const BASE_URL = 'http://localhost:8080';
 
 window.onload = () => {
--- html/sale.js
+++ html/sale.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://3.86.236.95:8080';
+const BASE_URL = 'https://www.maplestoryexchange.com';
 // const BASE_URL = 'http://localhost:8080';
 
 // 檢查 token 是否存在，若無則跳轉到 login.html
